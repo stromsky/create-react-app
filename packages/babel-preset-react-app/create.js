56e580150b35a3caff2dfebe7fe1dce228ecea54
Radek Stromský <radek.stromsky@linksoft.cz>
2019-05-23 09:50:50 +0200

    v2.0.2

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -141,7 +141,7 @@ module.exports = function(api, opts, env) {
         },
       ],
       // Turn on legacy decorators for TypeScript files
-      isTypeScriptEnabled && [
+      [
         require('@babel/plugin-proposal-decorators').default,
         false,
       ],
